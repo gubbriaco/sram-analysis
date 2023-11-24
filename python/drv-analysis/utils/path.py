238e7f7d1b3000d837ffd6abe023c7abcc0831e7
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2023-11-24 11:21:17 +0100

    spostamento cartelle in sram-6t

--- python/drv-analysis/utils/path.py
+++ python/drv-analysis/utils/path.py
@@ -2,8 +2,8 @@ ltspice = "../../ltspice/models/"
 schematics = "../../schematics/"
 images = "../../images/"
 
-rit_models_for_ltspice_file_path = (f"../../../../../ltspice/utils/utils-montecarlo-experiments"
+rit_models_for_ltspice_file_path = (f"../../../../../../ltspice/utils/utils-montecarlo-experiments"
                                     f"/RIT_Models_For_LTSPICE.txt")
 
 data = "./data/"
-snm_standard_data = f"{data}/snm-standard"
+sram_6t = f"{data}/sram-6t"
