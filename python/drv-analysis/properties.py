9b02fa0a3c002306388c3085c81be80a61094a29
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2023-11-25 17:01:27 +0100

    seevinck hold

--- python/drv-analysis/properties.py
+++ python/drv-analysis/properties.py
@@ -4,34 +4,84 @@ from utils.path import rit_models_for_ltspice_file_path
 # rit-models
 rit_models = f'.inc {rit_models_for_ltspice_file_path}'
 
+
+########################################################################################################################
+# SNM STANDARD
+
 # AX
-l_ax = '0.12u'
-w_ax_start = '0.12u'
-w_ax_stop = '0.24u'
-w_ax_step = '0.01u'
-w_ax_step_param = f'.step param w_ax {w_ax_start} {w_ax_stop} {w_ax_step}'
+l_ax_standard = '0.12u'
+w_ax_start_standard = '0.12u'
+w_ax_stop_standard = '0.24u'
+w_ax_step_standard = '0.01u'
+w_ax_step_param_standard = f'.step param w_ax {w_ax_start_standard} {w_ax_stop_standard} {w_ax_step_standard}'
 
 # save w_ax
-save_w_ax = '.meas w_ax_values param w_ax'
+save_w_ax_standard = '.meas w_ax_values param w_ax'
 
 # Q transistor
-l_pmos_q = '0.12u'
-w_pmos_q = '0.12u'
-l_nmos_q = '0.12u'
-w_nmos_q = '0.48u'
+l_pmos_q_standard = '0.12u'
+w_pmos_q_standard = '0.12u'
+l_nmos_q_standard = '0.12u'
+w_nmos_q_standard = '0.48u'
 
 # Q neg transistor
-l_pmos_q_neg = '0.12u'
-w_pmos_q_neg = '0.12u'
-l_nmos_q_neg = '0.12u'
-w_nmos_q_neg = '0.48u'
+l_pmos_q_neg_standard = '0.12u'
+w_pmos_q_neg_standard = '0.12u'
+l_nmos_q_neg_standard = '0.12u'
+w_nmos_q_neg_standard = '0.48u'
 
 # voltage
-vdd = '1'
-vsweep = '1'
+vdd_standard = '1'
+vsweep_standard = '1'
 
 # dc Vsweep
-dc_vsweep_start = 0
-dc_vsweep_stop = 1
-dc_vsweep_step = 0.01
-dc_vsweep = f'.dc Vsweep {dc_vsweep_start} {dc_vsweep_stop} {dc_vsweep_step}'
+dc_vsweep_start_standard = 0
+dc_vsweep_stop_standard = 1
+dc_vsweep_step_standard = 0.01
+dc_vsweep_standard = f'.dc Vsweep {dc_vsweep_start_standard} {dc_vsweep_stop_standard} {dc_vsweep_step_standard}'
+
+
+########################################################################################################################
+# SNM SEEVINCK
+
+# AX
+l_ax_seevinck = '0.12u'
+w_ax_start_seevinck = '0.12u'
+w_ax_stop_seevinck = '0.24u'
+w_ax_step_seevinck = '0.01u'
+w_ax_step_param_seevinck = f'.step param w_ax {w_ax_start_seevinck} {w_ax_stop_seevinck} {w_ax_step_seevinck}'
+
+# save w_ax
+save_w_ax_seevinck = '.meas w_ax_values param w_ax'
+
+# Q transistor
+l_pmos_q_seevinck = '0.12u'
+w_pmos_q_seevinck = '0.12u'
+l_nmos_q_seevinck = '0.12u'
+w_nmos_q_seevinck = '0.48u'
+
+# Q neg transistor
+l_pmos_q_neg_seevinck = '0.12u'
+w_pmos_q_neg_seevinck = '0.12u'
+l_nmos_q_neg_seevinck = '0.12u'
+w_nmos_q_neg_seevinck = '0.48u'
+
+# voltage
+vdd_seevinck = '1'
+vsweep_seevinck = '1'
+
+# dc Vsweep
+dc_vsweep_start_seevinck = -0.650
+dc_vsweep_stop_seevinck = 0.650
+dc_vsweep_step_seevinck = 0.01
+dc_vsweep_seevinck = f'.dc Vsweep {dc_vsweep_start_seevinck} {dc_vsweep_stop_seevinck} {dc_vsweep_step_seevinck}'
+
+# E1 E2 E3 E4 E5 E6 E7 E8
+e1 = '0.707'
+e2 = '0.707'
+e3 = '1'
+e4 = '1.41'
+e5 = '-1'
+e6 = '1.41'
+e7 = '0.707'
+e8 = '-0.707'
--- python/drv-analysis/utils/path.py
+++ python/drv-analysis/utils/path.py
@@ -2,8 +2,7 @@ ltspice = "../../ltspice/models/"
 schematics = "../../schematics/"
 images = "../../images/"
 
-rit_models_for_ltspice_file_path = (f"../../../../../../ltspice/utils/utils-montecarlo-experiments"
+rit_models_for_ltspice_file_path = (f"../../../../../ltspice/utils/utils-montecarlo-experiments"
                                     f"/RIT_Models_For_LTSPICE.txt")
 
-data = "./data/"
-sram_6t = f"{data}/sram-6t"
+data = "./data"
