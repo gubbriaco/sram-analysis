96ec47b21e6996d047312359982a02c38569149a
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2023-11-30 20:45:50 +0100

    modificati alcuni metodi

--- python/drv-analysis/models/ops.py
+++ python/drv-analysis/models/ops.py
@@ -1,18 +1,60 @@
-from utils.check import check_file
+from utils.check import check_file, check_image
 from PyLTSpice import RawRead, SpiceEditor
 from IPython.display import Image, display
+from matplotlib import pyplot
 
 
-def load_asc(asc_file_path, schematic_image_path):
+def load_asc(asc_file_path: str, schematic_image_path: str) -> SpiceEditor:
+    """
+    Carica un file .asc (netlist SPICE) e mostra l'immagine dello schema corrispondente.
+
+    :param asc_file_path: Percorso del file .asc contenente il netlist SPICE.
+    :param schematic_image_path: Percorso dell'immagine dello schema da visualizzare.
+    :return: Un oggetto SpiceEditor rappresentante il netlist caricato.
+    """
+
+    # Verifica che il file .asc esista
     check_file(asc_file_path)
+
+    # Carica il netlist SPICE dal file .asc
     netlist = SpiceEditor(asc_file_path)
 
+    # Visualizza l'immagine dello schema
     display(Image(schematic_image_path))
 
+    # Restituisci l'oggetto SpiceEditor contenente la netlist
     return netlist
 
 
-def load_ltr(raw_file_path):
+def load_ltr(raw_file_path: str) -> RawRead:
+    """
+    Carica un file .raw contenente dati di simulazione LTspice.
+
+    :param raw_file_path: Percorso del file .raw contenente i dati di simulazione LTspice.
+    :return: Un oggetto RawRead rappresentante i dati caricati dalla simulazione LTspice.
+    """
+
+    # Verifica che il file .raw esista
     check_file(raw_file_path)
+
+    # Carica i dati dalla simulazione LTspice dal file .raw
     ltr = RawRead(raw_file_path)
+
+    # Restituisci l'oggetto RawRead contenente i dati di simulazione
     return ltr
+
+
+def save_image(image_path: str, plt: pyplot) -> None:
+    """
+    Salva un'immagine utilizzando il percorso specificato e un oggetto pyplot di matplotlib.
+
+    :param image_path: Percorso in cui salvare l'immagine.
+    :param plt: Oggetto pyplot di matplotlib contenente l'immagine da salvare.
+    :return: None
+    """
+
+    # Verifica che il percorso e l'immagine siano validi
+    check_image(image_path)
+
+    # Salva l'immagine utilizzando l'oggetto pyplot
+    plt.savefig(image_path)
--- python/drv-analysis/models/snm.py
+++ python/drv-analysis/models/snm.py
@@ -3,20 +3,30 @@ from matplotlib import patches
 from scipy.interpolate import interp1d
 
 
-def standard_processing(x_vq, vq, x_vqneg, vqneg, ax, factor=100):
-    x1 = np.linspace(np.min(x_vq), np.max(x_vq), len(vq) * int(factor))
-    f = interp1d(x_vq, vq, kind='cubic')
-    y1 = f(x1)
+def rotate_points(x: float, y: float, angle_degrees: float) -> tuple[float, float]:
+    """
+    Ruota un punto attorno all'origine di un angolo specificato in gradi.
 
-    x2 = np.linspace(np.min(x_vqneg), np.max(x_vqneg), len(vqneg) * int(factor))
-    f = interp1d(x_vqneg, vqneg, kind='cubic')
-    y2 = f(x2)
+    :param x: Coordinata x del punto da ruotare (tipo float).
+    :param y: Coordinata y del punto da ruotare (tipo float).
+    :param angle_degrees: Angolo di rotazione in gradi (tipo float).
+    :return: Tuple contenente le coordinate x e y del punto ruotato.
 
-    ax.plot(x1, y1, color='blue')
-    ax.plot(x2, y2, color='green')
+    Questo metodo prende le coordinate (x, y) di un punto e lo ruota attorno all'origine di un angolo specificato in gradi.
+    Restituisce una tupla con le nuove coordinate (x_rotated, y_rotated) del punto ruotato.
+    """
+    angle_radians = np.radians(angle_degrees)
+    x_rotated = x * np.cos(angle_radians) - y * np.sin(angle_radians)
+    y_rotated = x * np.sin(angle_radians) + y * np.cos(angle_radians)
+    return x_rotated, y_rotated
+
+
+def graphical_processing(x_vq, vq, x_vqneg, vqneg, ax, factor=100):
+    x1, y1 = interpolate(x_v=x_vq, v=vq, factor=factor)
+    x2, y2 = interpolate(x_v=x_vqneg, v=vqneg, factor=factor)
+
     cross_index = np.argwhere(np.diff(np.sign(y1 - y2)) != 0).reshape(-1) + 0
     cross_index = cross_index[len(cross_index) // 2]
-    # ax.plot(x1[cross_index], y1[cross_index], 'go', color='red')
 
     if y1[cross_index - 1] < y2[cross_index + 1]:
         x1, y1, x2, y2 = x2, y2, x1, y1
@@ -40,40 +50,75 @@ def standard_processing(x_vq, vq, x_vqneg, vqneg, ax, factor=100):
 
     snm = 1000 * edge
 
-    ax.text(x[0] + edge + 0.05, y[0] + edge + 0.05, 'SNM= %.3f mV' % snm, fontsize=14)
-    ax.add_patch(patches.Rectangle((x[0], y[0]), width=edge, height=edge, fill=False))
-    ax.plot([x[0], x[0] + edge], [y[0], y[0] + edge], 'k')
-
-    ax.add_patch(
-        patches.Rectangle((y[0], x[0]), width=edge, height=edge, fill=False))
-    ax.plot([y[0], y[0] + edge], [x[0], x[0] + edge], 'k')
-
-    ax.grid()
-
-    ax.legend(["V(q)", "V(q_neg)"])
-    ax.set_title("Standard Method and SNM")
+    graphical_plot(
+        ax=ax,
+        x1=x1,
+        y1=y1,
+        x2=x2,
+        y2=y2,
+        edge=edge,
+        snm=snm,
+        x=x,
+        y=y
+    )
 
     return snm[0]
 
 
-def rotate_points(x, y, angle_degrees):
-    angle_radians = np.radians(angle_degrees)
-    x_rotated = x * np.cos(angle_radians) - y * np.sin(angle_radians)
-    y_rotated = x * np.sin(angle_radians) + y * np.cos(angle_radians)
-    return x_rotated, y_rotated
+def interpolate(x_v, v, factor):
+    x = np.linspace(start=np.min(x_v), stop=np.max(x_v), num=len(v) * int(factor))
+    f = interp1d(x_v, v, kind='cubic')
+    y = f(x)
+    return x, y
 
 
-def draw_inscribable_square(height, width, ax):
-    x_snm_start, y_snm_start = width, 0
-    x_snm_stop, y_snm_stop = width, height
+def graphical_plot(ax, x1, y1, x2, y2, edge, snm, x, y):
+    ax.plot(x1, y1, color='blue')
+    ax.plot(x2, y2, color='green')
 
-    x_diff_snm_start, y_diff_snm_start = rotate_points(x_snm_start, y_snm_start, -45)
-    x_diff_snm_stop, y_diff_snm_stop = rotate_points(x_snm_stop, y_snm_stop, -45)
-    ax.plot([x_diff_snm_start, x_diff_snm_stop], [y_diff_snm_start, y_diff_snm_stop], 'k')
-    snm = 1000 * height
-    # ax.text(x_snm_start + 0.05, y_snm_start + 0.05, 'SNM= %.3f mV' % snm, fontsize=14)
+    ax.text(x[0] + edge + 0.05, y[0] + edge + 0.05, 'SNM= %.3f mV' % snm, fontsize=14)
 
-    return snm, x_snm_start, y_snm_start
+    ax.add_patch(patches.Rectangle((x[0], y[0]), width=edge, height=edge, fill=False))
+    ax.plot([x[0], x[0] + edge], [y[0], y[0] + edge], 'k')
+
+    ax.add_patch(patches.Rectangle((y[0], x[0]), width=edge, height=edge, fill=False))
+    ax.plot([y[0], y[0] + edge], [x[0], x[0] + edge], 'k')
+
+    ax.grid()
+    ax.legend(["V(q)", "V(q_neg)"])
+    ax.set_title("Standard Method and SNM")
+
+
+def seevinck_processing(x_v1_minus_v2, v1_minus_v2, ax):
+    segments = find_segments(samples=v1_minus_v2)
+
+    height = max(segments[0])
+    i_width = np.where(v1_minus_v2 == height)[0]
+    width = x_v1_minus_v2[i_width]
+    (
+        snm,
+        x_snm_start,
+        y_snm_start,
+        x_diff_snm_start,
+        y_diff_snm_start,
+        x_diff_snm_stop,
+        y_diff_snm_stop
+    ) = inscribable_square(height=height, width=width)
+
+    x_diff, y_diff = rotate_points(x=x_v1_minus_v2, y=v1_minus_v2, angle_degrees=-45)
+
+    seevinck_plot(
+        ax=ax,
+        snm=snm,
+        x_snm_start=x_snm_start,
+        y_snm_start=y_snm_start,
+        x_diff=x_diff,
+        y_diff=y_diff,
+        x_snm_diff=[x_diff_snm_start, x_diff_snm_stop],
+        y_snm_diff=[y_diff_snm_start, y_diff_snm_stop]
+    )
+
+    return snm
 
 
 def find_segments(samples):
@@ -90,16 +135,29 @@ def find_segments(samples):
     return segments
 
 
-def seevinck_processing(x_v1_minus_v2, v1_minus_v2, ax):
-    segments = find_segments(v1_minus_v2)
+def inscribable_square(height, width):
+    x_snm_start, y_snm_start = width, 0
+    x_snm_stop, y_snm_stop = width, height
 
-    height = max(segments[0])
-    i_width = np.where(v1_minus_v2 == height)[0]
-    width = x_v1_minus_v2[i_width]
-    snm = draw_inscribable_square(height, width, ax)
+    x_diff_snm_start, y_diff_snm_start = rotate_points(x=x_snm_start, y=y_snm_start, angle_degrees=-45)
+    x_diff_snm_stop, y_diff_snm_stop = rotate_points(x=x_snm_stop, y=y_snm_stop, angle_degrees=-45)
 
-    x_diff, y_diff = rotate_points(x_v1_minus_v2, v1_minus_v2, -45)
+    snm = 1000 * height
+
+    return (
+        snm,
+        x_snm_start,
+        y_snm_start,
+        x_diff_snm_start,
+        y_diff_snm_start,
+        x_diff_snm_stop,
+        y_diff_snm_stop
+    )
+
+
+def seevinck_plot(ax, snm, x_snm_start, y_snm_start, x_diff, y_diff, x_snm_diff, y_snm_diff):
     ax.plot(x_diff, y_diff, 'red')
+    ax.plot(x_snm_diff, y_snm_diff, 'k')
+    ax.text(x_snm_start + 0.05, y_snm_start + 0.05, 'SNM= %.3f mV' % snm, fontsize=14)
+    ax.set_title("V(v1)-V(v2) Curve Hold Phase and SNM")
     ax.grid()
-
-    return snm
