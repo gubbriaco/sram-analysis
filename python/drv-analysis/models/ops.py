222fb430c52f74e3cfa99517c944fc5ec0eb8c51
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2023-12-03 18:02:01 +0100

    ileak vdd scaling aggiunto

--- python/drv-analysis/models/ops.py
+++ python/drv-analysis/models/ops.py
@@ -17,7 +17,7 @@ from properties import rit_models_montecarlo, l_ax_gaussian_vth, l_pmos_q_gaussi
     l_nmos_q_gaussian_vth, w_nmos_q_gaussian_vth, l_pmos_q_neg_gaussian_vth, w_pmos_q_neg_gaussian_vth, \
     l_nmos_q_neg_gaussian_vth, w_nmos_q_neg_gaussian_vth, dc_vsweep_gaussian_vth, vsweep_gaussian_vth, \
     e1_gaussian_vth, e2_gaussian_vth, e3_gaussian_vth, e4_gaussian_vth, e5_gaussian_vth, e6_gaussian_vth, \
-    e7_gaussian_vth, e8_gaussian_vth, step_param_run_gaussian_vth, w_ax_gaussian_vth
+    e7_gaussian_vth, e8_gaussian_vth, step_param_run_gaussian_vth, w_ax_gaussian_vth, tran_gaussian_vth
 from utils.path import data
 import re
 from PyLTSpice import SimRunner
@@ -89,7 +89,8 @@ def get_data(pattern: str, content: str) -> list[float]:
 class CircuitType(Enum):
     STANDARD = 0
     SEEVINCK = 1
-    GAUSSIAN_VTH = 2
+    GAUSSIAN_VTH_DC = 2
+    GAUSSIAN_VTH_TRANSIENT = 3
 
 
 class OperationType(Enum):
@@ -159,8 +160,20 @@ def __init_model__(
             vblneg=vblneg,
             params=params
         )
-    elif circuit_type == CircuitType.GAUSSIAN_VTH:
-        steps, x, ileaks, vq, vqneg, log = __init_gaussian_vth__(
+    elif circuit_type == CircuitType.GAUSSIAN_VTH_DC:
+        steps, x, ileaks, vq, vqneg, log = __init_gaussian_vth_dc__(
+            operation=operation,
+            asc_file_path=asc_file_path,
+            schematic_image_path=schematic_image_path,
+            vdd=vdd,
+            vsweep=vsweep,
+            vwl=vwl,
+            vbl=vbl,
+            vblneg=vblneg,
+            params=params
+        )
+    elif circuit_type == CircuitType.GAUSSIAN_VTH_TRANSIENT:
+        steps, x, ileaks, vq, vqneg, log = __init_gaussian_vth_transient__(
             operation=operation,
             asc_file_path=asc_file_path,
             schematic_image_path=schematic_image_path,
@@ -287,12 +300,12 @@ def __init_seevinck__(
     steps = seevinck_ltr.get_steps()
     log = seevinck_log
 
-    i_leaks_seevinck = []
+    i_leaks_gaussian_vth = []
 
-    return steps, v_sweep_seevinck, i_leaks_seevinck, v_1_seevinck, v_2_seevinck, log
+    return steps, v_sweep_seevinck, i_leaks_gaussian_vth, v_1_seevinck, v_2_seevinck, log
 
 
-def __init_gaussian_vth__(
+def __init_gaussian_vth_dc__(
         operation: str,
         asc_file_path: str,
         schematic_image_path: str,
@@ -303,57 +316,121 @@ def __init_gaussian_vth__(
         vblneg: str,
         params: list[str]
 ) -> tuple[list[int], np.ndarray, list[np.ndarray], np.ndarray, np.ndarray, str]:
-    gaussian_vth_netlist = load_asc(
+    gaussian_vth_dc_netlist = load_asc(
         asc_file_path=asc_file_path,
         schematic_image_path=schematic_image_path
     )
-    gaussian_vth_netlist.set_parameter('l_ax', l_ax_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('w_ax', w_ax_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('l_pmos_q', l_pmos_q_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('w_pmos_q', w_pmos_q_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('l_nmos_q', l_nmos_q_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('w_nmos_q', w_nmos_q_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('l_pmos_q_neg', l_pmos_q_neg_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('w_pmos_q_neg', w_pmos_q_neg_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('l_nmos_q_neg', l_nmos_q_neg_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('w_nmos_q_neg', w_nmos_q_neg_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('vdd', vdd)
-    gaussian_vth_netlist.set_parameter('vwl', vwl)
-    gaussian_vth_netlist.set_parameter('vbl', vbl)
-    gaussian_vth_netlist.set_parameter('vblneg', vblneg)
-    gaussian_vth_netlist.set_parameter('vsweep', vsweep)
-    gaussian_vth_netlist.set_parameter('e1', e1_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e2', e2_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e3', e3_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e4', e4_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e5', e5_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e6', e6_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e7', e7_gaussian_vth)
-    gaussian_vth_netlist.set_parameter('e8', e8_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('l_ax', l_ax_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('w_ax', w_ax_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('l_pmos_q', l_pmos_q_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('w_pmos_q', w_pmos_q_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('l_nmos_q', l_nmos_q_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('w_nmos_q', w_nmos_q_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('l_pmos_q_neg', l_pmos_q_neg_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('w_pmos_q_neg', w_pmos_q_neg_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('l_nmos_q_neg', l_nmos_q_neg_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('w_nmos_q_neg', w_nmos_q_neg_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('vdd', vdd)
+    gaussian_vth_dc_netlist.set_parameter('vwl', vwl)
+    gaussian_vth_dc_netlist.set_parameter('vbl', vbl)
+    gaussian_vth_dc_netlist.set_parameter('vblneg', vblneg)
+    gaussian_vth_dc_netlist.set_parameter('vsweep', vsweep)
+    gaussian_vth_dc_netlist.set_parameter('e1', e1_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e2', e2_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e3', e3_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e4', e4_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e5', e5_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e6', e6_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e7', e7_gaussian_vth)
+    gaussian_vth_dc_netlist.set_parameter('e8', e8_gaussian_vth)
     for param in params:
-        gaussian_vth_netlist.add_instructions(param)
+        gaussian_vth_dc_netlist.add_instructions(param)
 
-    gaussian_vth_runner = SimRunner(output_folder=f"{data}/gaussian-vth/{operation}/")
-    gaussian_vth_runner.run(netlist=gaussian_vth_netlist, timeout=3600)
-    print('Successful/Total Simulations: ' + str(gaussian_vth_runner.okSim) + '/' + str(
-        gaussian_vth_runner.runno))
+    gaussian_vth_dc_runner = SimRunner(output_folder=f"{data}/gaussian-vth/{operation}/")
+    gaussian_vth_dc_runner.run(netlist=gaussian_vth_dc_netlist, timeout=3600)
+    print('Successful/Total Simulations: ' + str(gaussian_vth_dc_runner.okSim) + '/' + str(
+        gaussian_vth_dc_runner.runno))
 
-    gaussian_vth_raw = ""
-    gaussian_vth_log = ""
-    for gaussian_vth_raw, gaussian_vth_log in gaussian_vth_runner:
-        print("Raw file: %s, Log file: %s" % (gaussian_vth_raw, gaussian_vth_log))
+    gaussian_vth_dc_raw = ""
+    gaussian_vth_dc_log = ""
+    for gaussian_vth_dc_raw, gaussian_vth_dc_log in gaussian_vth_dc_runner:
+        print("Raw file: %s, Log file: %s" % (gaussian_vth_dc_raw, gaussian_vth_dc_log))
 
-    gaussian_vth_ltr = load_ltr(raw_file_path=gaussian_vth_raw)
-    v_1_gaussian_vth = gaussian_vth_ltr.get_trace("V(v1)")
-    v_2_gaussian_vth = gaussian_vth_ltr.get_trace("V(v2)")
-    v_sweep_gaussian_vth = gaussian_vth_ltr.get_trace('time')
-    steps = gaussian_vth_ltr.get_steps()
+    gaussian_vth_dc_ltr = load_ltr(raw_file_path=gaussian_vth_dc_raw)
+    v_1_gaussian_dc_vth = gaussian_vth_dc_ltr.get_trace("V(v1)")
+    v_2_gaussian_dc_vth = gaussian_vth_dc_ltr.get_trace("V(v2)")
+    v_sweep_gaussian_vth_dc = gaussian_vth_dc_ltr.get_trace('vsweep')
+    steps = gaussian_vth_dc_ltr.get_steps()
 
-    log = gaussian_vth_log
+    log = gaussian_vth_dc_log
 
-    i_leaks_gaussian_vth = get_ileaks(gaussian_vth_ltr, operation)
+    i_leaks_gaussian_vth_dc = []
 
-    return steps, v_sweep_gaussian_vth, i_leaks_gaussian_vth, v_1_gaussian_vth, v_2_gaussian_vth, log
+    return steps, v_sweep_gaussian_vth_dc, i_leaks_gaussian_vth_dc, v_1_gaussian_dc_vth, v_2_gaussian_dc_vth, log
+
+
+def __init_gaussian_vth_transient__(
+        operation: str,
+        asc_file_path: str,
+        schematic_image_path: str,
+        vdd: str,
+        vsweep: str,
+        vwl: str,
+        vbl: str,
+        vblneg: str,
+        params: list[str]
+) -> tuple[list[int], np.ndarray, list[np.ndarray], np.ndarray, np.ndarray, str]:
+    gaussian_vth_transient_netlist = load_asc(
+        asc_file_path=asc_file_path,
+        schematic_image_path=schematic_image_path
+    )
+    gaussian_vth_transient_netlist.set_parameter('l_ax', l_ax_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('w_ax', w_ax_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('l_pmos_q', l_pmos_q_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('w_pmos_q', w_pmos_q_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('l_nmos_q', l_nmos_q_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('w_nmos_q', w_nmos_q_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('l_pmos_q_neg', l_pmos_q_neg_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('w_pmos_q_neg', w_pmos_q_neg_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('l_nmos_q_neg', l_nmos_q_neg_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('w_nmos_q_neg', w_nmos_q_neg_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('vdd', vdd)
+    gaussian_vth_transient_netlist.set_parameter('vwl', vwl)
+    gaussian_vth_transient_netlist.set_parameter('vbl', vbl)
+    gaussian_vth_transient_netlist.set_parameter('vblneg', vblneg)
+    gaussian_vth_transient_netlist.set_parameter('vsweep', vsweep)
+    gaussian_vth_transient_netlist.set_parameter('e1', e1_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e2', e2_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e3', e3_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e4', e4_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e5', e5_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e6', e6_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e7', e7_gaussian_vth)
+    gaussian_vth_transient_netlist.set_parameter('e8', e8_gaussian_vth)
+    for param in params:
+        gaussian_vth_transient_netlist.add_instructions(param)
+
+    gaussian_vth_transient_runner = SimRunner(output_folder=f"{data}/gaussian-vth/{operation}/")
+    gaussian_vth_transient_runner.run(netlist=gaussian_vth_transient_netlist, timeout=3600)
+    print('Successful/Total Simulations: ' + str(gaussian_vth_transient_runner.okSim) + '/' + str(
+        gaussian_vth_transient_runner.runno))
+
+    gaussian_vth_transient_raw = ""
+    gaussian_vth_transient_log = ""
+    for gaussian_vth_transient_raw, gaussian_vth_transient_log in gaussian_vth_transient_runner:
+        print("Raw file: %s, Log file: %s" % (gaussian_vth_transient_raw, gaussian_vth_transient_log))
+
+    gaussian_vth_transient_ltr = load_ltr(raw_file_path=gaussian_vth_transient_raw)
+    v_1_gaussian_vth_transient = gaussian_vth_transient_ltr.get_trace("V(v1)")
+    v_2_gaussian_vth_transient = gaussian_vth_transient_ltr.get_trace("V(v2)")
+    time = gaussian_vth_transient_ltr.get_trace('time')
+    steps = gaussian_vth_transient_ltr.get_steps()
+
+    log = gaussian_vth_transient_log
+
+    i_leaks_gaussian_vth_transient = get_ileaks(gaussian_vth_transient_ltr, operation)
+
+    return steps, time, i_leaks_gaussian_vth_transient, v_1_gaussian_vth_transient, v_2_gaussian_vth_transient, log
 
 
 def get_ileaks(ltr: RawRead, operation: str) -> list[np.ndarray]:
